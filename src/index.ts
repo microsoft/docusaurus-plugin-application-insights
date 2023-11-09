bc125b816cc1d459e229934c0c7c3c792c6e5cde
Steve Calvert <li_scalvert@microsoft.com>
2023-11-09 14:53:12 -0800

    Adds copyright to source and CONTRIBUTING.md (#4)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import type { LoadContext, Plugin } from '@docusaurus/types';
 import { PluginOptions, Options, normalizeConfig } from './options';
 import { resolve } from 'node:path';
